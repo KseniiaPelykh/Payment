de18933c8a8a8fc14392217e56eaa11b5a98c244
Kseniia Pelykh <kseniia.pelykh@ynap.com>
2019-11-18 19:50:16 +0100

    removing logging comment            }

--- src/Payment.Core/PaymentProcessing.cs
+++ src/Payment.Core/PaymentProcessing.cs
@@ -22,22 +22,15 @@ namespace Payment.Core
 
             var paymentId = PaymentId.GenerateNew();
 
-            try
-            {
-                var payment = new Payment(
-                    paymentId: paymentId,
-                    cardNumber: Masking.GetMask(request.CardNumber),
-                    amount: request.Amount,
-                    currency: request.Currency,
-                    operationDate: DateTimeOffset.UtcNow,
-                    bankAuthorizationResult: authorizationResult);
+            var payment = new Payment(
+                paymentId: paymentId,
+                cardNumber: Masking.GetMask(request.CardNumber),
+                amount: request.Amount,
+                currency: request.Currency,
+                operationDate: DateTimeOffset.UtcNow,
+                bankAuthorizationResult: authorizationResult);
 
-                await _repository.PutAsync(payment);
-            }
-            catch (Exception e)
-            {
-                //write to log that it some problem with storing request
-            }
+            await _repository.PutAsync(payment);
 
             return authorizationResult.IsSuccess
                 ? Result<PaymentId>.CreateSuccess(paymentId)
